ed768c17675cffbc1a57af9355f12200f3a69468
salybu <rkdmsdl93@gmail.com>
2021-07-18 20:55:06 +0900

    Aside 컴포넌트 - 기업 서비스 버튼 추가

--- src/Headers/Aside.jsx
+++ src/Headers/Aside.jsx
@@ -1,7 +1,12 @@
 import React from "react";
 import Box from "@material-ui/core/Box";
 import IconButton from "@material-ui/core/IconButton";
-import { createTheme, withStyles, makeStyles } from "@material-ui/core/styles";
+import {
+  createTheme,
+  withStyles,
+  makeStyles,
+  ThemeProvider,
+} from "@material-ui/core/styles";
 import Avatar from "@material-ui/core/Avatar";
 import Button from "@material-ui/core/Button";
 import SearchRoundedIcon from "@material-ui/icons/SearchRounded";
@@ -19,6 +24,25 @@ const MyButton = withStyles({
   },
 })(Button);
 
+const CorpButton = withStyles({
+  root: {
+    width: 90,
+    height: 30,
+    border: "1px solid #e1e2e3",
+    borderRadius: 20,
+    fontSize: 13,
+    float: "right",
+    "&::before": {
+      // display: "block",
+      content: "",
+      width: 4,
+      height: 10,
+      backgroundColor: "#e1e2e3",
+      margin: "auto 10px",
+    },
+  },
+})(Button);
+
 const useStyles = makeStyles((theme) => ({
   iconButton: {
     // disableFocusRipple: true,
@@ -26,32 +50,48 @@ const useStyles = makeStyles((theme) => ({
     // disabled: true,
     padding: 5,
   },
+  corpService: {
+    width: 127,
+  },
 }));
 
-export default function Aside() {
+const theme = createTheme({
+  props: {
+    MuiButtonBase: {
+      disableRipple: true,
+      disableTouchRipple: true,
+    },
+  },
+});
+
+export default function Aside(props) {
   const classes = useStyles();
 
   return (
-    <div style={{ padding: "9px 0px" }}>
-      <Box display="flex" style={{ width: 260 }}>
-        <IconButton className={classes.iconButton}>
-          <SearchRoundedIcon />
-        </IconButton>
-        <IconButton className={classes.iconButton}>
-          <NotificationsNoneRoundedIcon />
-        </IconButton>
-        <div style={{ width: 35, marginLeft: 8 }}>
-          <MyButton>
-            <Badge badgeContent={4} color="primary">
-              <Avatar
-                src={ProfileImg}
-                style={{ borderRadius: 20, width: 28, height: 28 }}
-              />
-            </Badge>
-          </MyButton>
-        </div>
-        <Box></Box>
-      </Box>
-    </div>
+    <ThemeProvider theme={theme}>
+      <div style={{ padding: "9px 0px" }}>
+        <Box display="flex" style={{ width: 260 }}>
+          <IconButton className={classes.iconButton} onClick={props.showSearch}>
+            <SearchRoundedIcon />
+          </IconButton>
+          <IconButton className={classes.iconButton}>
+            <NotificationsNoneRoundedIcon />
+          </IconButton>
+          <div style={{ width: 35, marginLeft: 8 }}>
+            <MyButton>
+              <Badge badgeContent={4} color="primary">
+                <Avatar
+                  src={ProfileImg}
+                  style={{ borderRadius: 20, width: 28, height: 28 }}
+                />
+              </Badge>
+            </MyButton>
+          </div>
+          <div className={classes.corpService}>
+            <CorpButton>기업 서비스</CorpButton>
+          </div>
+        </Box>
+      </div>
+    </ThemeProvider>
   );
 }

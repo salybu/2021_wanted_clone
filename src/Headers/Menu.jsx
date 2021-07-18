691c58ffd0cfa32f5a7c47b386ad4ab7eca7ad24
salybu <rkdmsdl93@gmail.com>
2021-07-18 21:00:02 +0900

    글씨체 추가 및 적용, 메뉴 로직 일부 수정

--- src/Headers/Menu.jsx
+++ src/Headers/Menu.jsx
@@ -1,16 +1,35 @@
+import "../index.css";
+
 import React from "react";
+import { useContext } from "react";
 import { makeStyles } from "@material-ui/core/styles";
 import { createTheme, ThemeProvider } from "@material-ui/core/styles";
 import Box from "@material-ui/core/Box";
 import Grid from "@material-ui/core/Grid";
+import { blueGrey } from "@material-ui/core/colors";
+import { CssBaseline } from "@material-ui/core";
+// import HeaderContext from "../contexts/HeaderContext";
+
+// const theme = createTheme({
+//   props: {
+//     // Style sheet name ⚛️
+//     MuiGrid: {
+//       // Name of the rule
+//       item: {
+//         padding: 0,
+//       },
+//     },
+//   },
+// });
 
 const theme = createTheme({
-  props: {
-    // Style sheet name ⚛️
-    MuiGrid: {
-      // Name of the rule
-      item: {
-        padding: 0,
+  typography: {
+    fontFamily: ["Gothic A1", "Gothic A1 Light", "sans-serif"].join(","),
+  },
+  overrides: {
+    MuiCssBaseline: {
+      "@global": {
+        "@font-face": ["Gothic A1", "Gothic A1 Light", "sans-serif"],
       },
     },
   },
@@ -22,40 +41,62 @@ const useStyles = makeStyles((theme) => ({
     padding: theme.spacing(0),
   },
   item: {
+    fontFamily: "Gothic A1",
     padding: 15,
     textAlign: "center",
+    fontWeight: "bold",
     // color: theme.palette.text.secondary,
+  },
+  beta: {
+    fontFamily: "Gothic A1 Light",
+    fontWeight: "100",
+    position: "absolute",
+    color: "#189CC4",
+    fontSize: 10,
+    top: 10,
+    right: -7,
   },
 }));
 
-export default function Menu() {
+export default function Menu(props) {
   const classes = useStyles();
 
   return (
-    <div className={classes.root}>
-      <Grid container>
-        <Grid item>
-          <Box className={classes.item}>탐색</Box>
+    <ThemeProvider theme={theme}>
+      <CssBaseline />
+      <div className={classes.root}>
+        <Grid container>
+          <Grid item>
+            <Box
+              className={classes.item}
+              // onMouseOver={props.setState}
+              // onMouseOut={props.setState}
+              onMouseOver={props.showSlideMenu}
+            >
+              탐색
+            </Box>
+          </Grid>
+          {["커리어 성장", "직군별 연봉", "이력서", "매치업", "프리랜서"].map(
+            (itemName) => (
+              <Grid item>
+                <Box className={classes.item} onMouseOver={props.hideSlideMenu}>
+                  {itemName}
+                </Box>
+              </Grid>
+            )
+          )}
+          <Grid item>
+            <Box
+              className={classes.item}
+              onMouseOver={props.hideSlideMenu}
+              style={{ position: "relative" }}
+            >
+              Ai 합격예측
+              <div className={classes.beta}>Beta</div>
+            </Box>
+          </Grid>
         </Grid>
-        <Grid item>
-          <Box className={classes.item}>커리어 성장</Box>
-        </Grid>
-        <Grid item>
-          <Box className={classes.item}>직군별 연봉</Box>
-        </Grid>
-        <Grid item>
-          <Box className={classes.item}>이력서</Box>
-        </Grid>
-        <Grid item>
-          <Box className={classes.item}>매치업</Box>
-        </Grid>
-        <Grid item>
-          <Box className={classes.item}>프리랜서</Box>
-        </Grid>
-        <Grid item>
-          <Box className={classes.item}>Ai 합격예측</Box>
-        </Grid>
-      </Grid>
-    </div>
+      </div>
+    </ThemeProvider>
   );
 }

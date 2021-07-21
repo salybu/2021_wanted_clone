745caf017ce313efa1d49e4a7e643a1aaa8b2a6d
salybu <rkdmsdl93@gmail.com>
2021-07-21 17:47:56 +0900

    Aside 컴포넌트: 주석 정리

--- src/Headers/Aside.jsx
+++ src/Headers/Aside.jsx
@@ -1,29 +1,33 @@
 import React from "react";
+
 import Box from "@material-ui/core/Box";
+import Button from "@material-ui/core/Button";
 import IconButton from "@material-ui/core/IconButton";
+import Avatar from "@material-ui/core/Avatar";
+import Badge from "@material-ui/core/Badge";
+import List from "@material-ui/core/List";
+import ListItem from "@material-ui/core/ListItem";
+import ListItemText from "@material-ui/core/ListItemText";
+import Divider from "@material-ui/core/Divider";
+import Menu from "@material-ui/core/Menu";
+import MenuItem from "@material-ui/core/MenuItem";
+import useMediaQuery from "@material-ui/core/useMediaQuery";
+
+import { useState } from "react";
 import {
   createTheme,
   withStyles,
   makeStyles,
   ThemeProvider,
 } from "@material-ui/core/styles";
-import Avatar from "@material-ui/core/Avatar";
-import Button from "@material-ui/core/Button";
+
 import SearchRoundedIcon from "@material-ui/icons/SearchRounded";
 import NotificationsNoneRoundedIcon from "@material-ui/icons/NotificationsNoneRounded";
 import SubjectRoundedIcon from "@material-ui/icons/SubjectRounded";
+
 import ProfileImg from "../avatar.jpg";
-import Badge from "@material-ui/core/Badge";
-import { useState } from "react";
 
-import List from "@material-ui/core/List";
-import ListItem from "@material-ui/core/ListItem";
-import Divider from "@material-ui/core/Divider";
-import ListItemText from "@material-ui/core/ListItemText";
-import Menu from "@material-ui/core/Menu";
-import MenuItem from "@material-ui/core/MenuItem";
-import useMediaQuery from "@material-ui/core/useMediaQuery";
-
+// 알람 아이콘 클릭 시 출력, 알람 리스트
 const alarmList = [
   {
     category: "커리어 성장",
@@ -51,6 +55,7 @@ const alarmList = [
   },
 ];
 
+// 프로필(아바타) 아이콘 클릭 시 출력, 메뉴 리스트
 const profileMenuList = [
   "MY 원티드",
   "프로필",
@@ -62,52 +67,9 @@ const profileMenuList = [
   "포인트",
 ];
 
-const ProfileButton = withStyles({
-  root: {
-    maxWidth: 32,
-    minWidth: 32,
-    height: 32,
-    border: "1px solid #e1e2e3",
-    borderRadius: 16,
-  },
-})(Button);
-
-const NewBadge = withStyles((theme) => ({
-  badge: {
-    padding: "0 4px",
-    backgroundColor: "#3366FF",
-    color: "#f8f8fa",
-    fontSize: 7,
-    fontWeight: 600,
-    top: 3,
-    right: 2,
-    height: 15,
-    minWidth: 11,
-  },
-}))(Badge);
-
-const MyBadge = withStyles((theme) => ({
-  badge: {
-    backgroundColor: "#3366FF",
-    height: 5,
-    minWidth: 5,
-  },
-}))(Badge);
-
-const CorpButton = withStyles({
-  root: {
-    width: 90,
-    height: 30,
-    border: "1px solid #e1e2e3",
-    borderRadius: 20,
-    fontSize: 13,
-    float: "right",
-  },
-})(Button);
-
+// 알람 리스트 상위 컴포넌트
 const AlarmMenu = withStyles({
   paper: {
-    // border: "1px solid #cdcdcd",
     borderRadius: 15,
     boxShadow:
       "rgba(60, 64, 67, 0.3) 0px 1px 2px 0px, rgba(60, 64, 67, 0.15) 0px 2px 6px 2px",
@@ -128,11 +90,47 @@ const AlarmMenu = withStyles({
   />
 ));
 
+// 프로필(아바타) 버튼
+const ProfileButton = withStyles({
+  root: {
+    maxWidth: 32,
+    minWidth: 32,
+    height: 32,
+    border: "1px solid #e1e2e3",
+    borderRadius: 16,
+  },
+})(Button);
+
+// 프로필(아바타) 우측상단에 New 알림 뱃지
+const NewBadge = withStyles((theme) => ({
+  badge: {
+    padding: "0 4px",
+    backgroundColor: "#3366FF",
+    color: "#f8f8fa",
+    fontSize: 7,
+    fontWeight: 600,
+    top: 3,
+    right: 2,
+    height: 15,
+    minWidth: 11,
+  },
+}))(Badge);
+
+// 프로필 메뉴 내부에 알림 뱃지
+const ProfileMenuBadge = withStyles((theme) => ({
+  badge: {
+    backgroundColor: "#3366FF",
+    height: 5,
+    minWidth: 5,
+    top: 4,
+    right: -2,
+  },
+}))(Badge);
+
 const ProfileMenu = withStyles({
   paper: {
     width: "192px",
     maxHeight: "415px",
-    // border: "1px solid #cdcdcd",
     borderRadius: 15,
     boxShadow:
       "rgba(60, 64, 67, 0.3) 0px 1px 2px 0px, rgba(60, 64, 67, 0.15) 0px 2px 6px 2px",
@@ -169,6 +167,18 @@ const ProfileMenuItem = withStyles(() => ({
   },
 }))(MenuItem);
 
+// 기업 서비스 (버튼으로 구현)
+const CorpButton = withStyles({
+  root: {
+    width: 90,
+    height: 30,
+    border: "1px solid #e1e2e3",
+    borderRadius: 20,
+    fontSize: 13,
+    float: "right",
+  },
+})(Button);
+
 const theme = createTheme({
   props: {
     MuiButtonBase: {
@@ -176,44 +186,20 @@ const theme = createTheme({
       disableTouchRipple: true,
     },
   },
-  breakpoints: {
-    values: {
-      xs: 0,
-      sm: 690,
-      md: 960,
-      lg: 1280,
-      xl: 1920,
-    },
-  },
 });
 
 const useStyles = makeStyles((theme) => ({
+  // 아이콘 버튼 (검색, 알람)
   iconButton: {
-    // disableFocusRipple: true,
-    // disableRipple: true,
-    // disabled: true,
     padding: 5,
   },
+  // 아이콘 버튼 (검색, 알람): 반응형 적용 후
   iconButtonMobile: {
     minWidth: 20,
     height: 20,
     padding: "0 5px",
   },
-  corpService: {
-    display: "flex",
-    width: 127,
-    justifyContent: "space-between",
-    "&::before": {
-      // display: "block",
-      content: "''",
-      minWidth: 1,
-      maxWidth: 1,
-      minHeight: 10,
-      maxHeight: 10,
-      backgroundColor: "#e1e2e3",
-      margin: "10px 10px 10px 20px",
-    },
-  },
+  // 알람 메뉴 리스트
   alarmMenuWrap: {
     margin: 0,
     width: "360px",
@@ -244,25 +230,32 @@ const useStyles = makeStyles((theme) => ({
       },
     },
   },
+  corpService: {
+    display: "flex",
+    width: 127,
+    justifyContent: "space-between",
+    "&::before": {
+      content: "''",
+      minWidth: 1,
+      maxWidth: 1,
+      minHeight: 10,
+      maxHeight: 10,
+      backgroundColor: "#e1e2e3",
+      margin: "10px 10px 10px 20px",
+    },
+  },
 }));
 
 export default function Aside(props) {
   const classes = useStyles();
-  // const [anchorEl, setAnchorEl] = React.useState(null);
-
-  // const handleClick = (event) => {
-  //   setAnchorEl(event.currentTarget);
-  // };
-
-  // const handleClose = () => {
-  //   setAnchorEl(null);
-  // };
-  const showAllMenu = useMediaQuery("(min-width:770px)");
+  const showAllMenu = useMediaQuery("(min-width:770px)"); // 반응형 기준 구분용 변수
 
   const [state, setState] = useState({
     alarmAnchorEl: null,
     profileAnchorEl: null,
   });
+
+  // 알람메뉴 Open, Close 함수
   const alarmOpen = (event) => {
     setState({ alarmAnchorEl: event.currentTarget });
   };
@@ -271,6 +264,7 @@ export default function Aside(props) {
     setState({ alarmAnchorEl: null });
   };
 
+  // 프로필메뉴 Open, Close 함수
   const profileOpen = (event) => {
     setState({ profileAnchorEl: event.currentTarget });
   };
@@ -282,8 +276,7 @@ export default function Aside(props) {
   return (
     <ThemeProvider theme={theme}>
       <div style={{ padding: "9px 0px" }}>
-        {console.log(theme.breakpoints)}
-        {showAllMenu ? (
+        {showAllMenu ? ( // width: 690px 이상일 때
           <Box display="flex" style={{ width: 260 }}>
             <IconButton
               className={classes.iconButton}
@@ -291,15 +284,14 @@ export default function Aside(props) {
             >
               <SearchRoundedIcon />
             </IconButton>
+            {/* 알람 버튼, 알람 메뉴 */}
             <IconButton className={classes.iconButton} onClick={alarmOpen}>
               <NotificationsNoneRoundedIcon />
             </IconButton>
             <AlarmMenu
               id="alarm-menu"
-              // anchorEl={anchorEl}
               anchorEl={state.alarmAnchorEl}
               keepMounted
-              // open={Boolean(anchorEl)}
               open={Boolean(state.alarmAnchorEl)}
               onClose={alarmClose}
             >
@@ -320,13 +312,14 @@ export default function Aside(props) {
                         />
                       </Button>
                     </ListItem>
-                    {alarmList[index + 1] && ( // 마지막 구분선 생략
+                    {alarmList[index + 1] && ( // 알람 메뉴 리스트의 마지막 구분선 생략
                       <Divider variant="inset" component="li" />
                     )}
                   </React.Fragment>
                 ))}
               </List>
             </AlarmMenu>
+            {/* 프로필 버튼, 프로필 메뉴 */}
             <div style={{ width: 35, marginLeft: 8 }}>
               <ProfileButton onClick={profileOpen}>
                 <NewBadge badgeContent="N">
@@ -345,16 +338,17 @@ export default function Aside(props) {
               >
                 {profileMenuList.map((profileMenu, index) => (
                   <React.Fragment>
-                    {index == 0 ? (
+                    {index == 0 ? ( // MY 원티드 메뉴에 뱃지를 함께 출력 (클론 코딩용)
                       <ProfileMenuItem>
-                        <MyBadge color="secondary" variant="dot">
+                        <ProfileMenuBadge color="secondary" variant="dot">
                           {profileMenu}
-                        </MyBadge>
+                        </ProfileMenuBadge>
                       </ProfileMenuItem>
                     ) : (
+                      // 뱃지가 없는 메뉴는 그대로 출력
                       <ProfileMenuItem>{profileMenu}</ProfileMenuItem>
                     )}
-                    {(index == 1 || index == 5) && (
+                    {(index == 1 || index == 5) && ( // 구분선 위치에 맞게 출력
                       <Divider variant="inset" component="li" />
                     )}
                   </React.Fragment>
@@ -370,11 +364,13 @@ export default function Aside(props) {
                 </ProfileMenuItem>
               </ProfileMenu>
             </div>
+            {/* 기업 서비스 버튼 */}
             <div className={classes.corpService}>
               <CorpButton>기업 서비스</CorpButton>
             </div>
           </Box>
         ) : (
+          // width 690px 이하일 때
           <Box
             display="flex"
             style={{

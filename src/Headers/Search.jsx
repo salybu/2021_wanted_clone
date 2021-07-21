93b07d2b34d4d3c607ad3e05b1d420ddf436cbba
salybu <rkdmsdl93@gmail.com>
2021-07-21 16:59:01 +0900

    Search 컴포넌트 - 반응형 구현, 이벤트 버블링 해제, 변수명 정리 등

--- src/Headers/Search.jsx
+++ src/Headers/Search.jsx
@@ -1,12 +1,15 @@
 import React from "react";
-import { makeStyles, TextField, withStyles } from "@material-ui/core";
-import Container from "@material-ui/core/Container";
-import SearchRoundedIcon from "@material-ui/icons/SearchRounded";
-import Link from "@material-ui/core/Link";
-import Button from "@material-ui/core/Button";
-import NavigateNextRoundedIcon from "@material-ui/icons/NavigateNextRounded";
-import { PinDropSharp } from "@material-ui/icons";
 
+import Container from "@material-ui/core/Container";
+import Button from "@material-ui/core/Button";
+import Link from "@material-ui/core/Link";
+
+import { makeStyles, TextField, withStyles } from "@material-ui/core";
+
+import NavigateNextRoundedIcon from "@material-ui/icons/NavigateNextRounded";
+import SearchRoundedIcon from "@material-ui/icons/SearchRounded";
+
+// 추천태그
 const tagRecommend = [
   { tag: "#육아휴직", link: "#button1", color: "#f0f8f8" },
   { tag: "#퇴사율5%이하", link: "#button2", color: "#eeedf4" },
@@ -15,7 +18,36 @@ const tagRecommend = [
   { tag: "#반려동물", link: "#button5", color: "#effbf3" },
 ];
 
+// 검색창
+const SearchTextField = withStyles({
+  root: {
+    "& .MuiOutlinedInput-root": {
+      "&": {
+        borderRadius: 30,
+        backgroundColor: "#f2f4f7",
+      },
+      "& :focus": {
+        backgroundColor: "#fff",
+      },
+      "& input": {
+        padding: "15px 36px 15px 60px",
+      },
+    },
+  },
+})(TextField);
+
+// 각각의 추천태그 (버튼으로 구현)
+const TagButton = withStyles({
+  root: {
+    height: 50,
+    fontSize: 15,
+    borderRadius: 25,
+    padding: "0 26px",
+  },
+})(Button);
+
 const useStyles = makeStyles((theme) => ({
+  // 전체 감싸는 div
   wrap: {
     position: "absolute",
     width: "100%",
@@ -24,20 +56,24 @@ const useStyles = makeStyles((theme) => ({
     backgroundColor: "#fff",
     zIndex: 101,
   },
-  search: {
-    // flexGrow: 1,
+  // 검색 부분
+  searchContainer: {
     maxWidth: 1060,
     padding: "30px 0 100px",
     margin: "0 auto",
-  },
-  noBorder: {
-    border: "none",
+    [theme.breakpoints.between("sm", "md")]: {
+      width: "90%",
+    },
+    [theme.breakpoints.down("sm")]: {
+      padding: 20,
+    },
   },
   iconSearch: {
     position: "absolute",
     top: 14,
     left: 20,
   },
+  // 추천태그 부분
   tagWrap: {
     position: "fixed",
     left: 0,
@@ -53,18 +89,24 @@ const useStyles = makeStyles((theme) => ({
       color: "#333333",
       display: "inline-block",
     },
+    [theme.breakpoints.between("sm", "md")]: {
+      width: "90%",
+    },
+    [theme.breakpoints.down("sm")]: {
+      padding: "40px 20px 20px",
+    },
   },
-  moveTagHome: {
+  moveCorpTagHome: {
     float: "right",
     position: "relative",
     right: 15,
   },
-  moveIcon: {
+  moveCorpTagIcon: {
     width: 19,
     height: 19,
     position: "absolute",
   },
-  tagRecommend: {
+  tagRecommendWrap: {
     listStyle: "none",
     margin: 0,
     padding: "20px 0 50px",
@@ -72,97 +114,46 @@ const useStyles = makeStyles((theme) => ({
     "& li": {
       float: "left",
       marginLeft: 10,
+      "&:first-child": {
+        margin: 0,
+      },
     },
-    "& li:first-child": {
-      margin: 0,
+    // 반응형 구현, theme. 를 사용하기 위해 const TagButton 대신 useStyles 이용
+    [theme.breakpoints.down("sm")]: {
+      "& a": {
+        // TagButton
+        height: 34,
+        padding: 12,
+      },
+      "& .MuiButton-label": {
+        // TagButton 제목
+        fontSize: 13,
+      },
     },
-    // "&::after": {
-    //   content: " ",
-    //   display: "block",
-    //   clear: "both",
-    // },
   },
 }));
 
-const TagButton = withStyles({
-  root: {
-    height: 50,
-    // lineHeight: 50,
-    fontSize: 15,
-    borderRadius: 25,
-    padding: "0 26px",
-  },
-})(Button);
-
-const SearchTextField = withStyles({
-  root: {
-    "& .MuiOutlinedInput-root": {
-      "&": {
-        borderRadius: 30,
-        // borderColor: "#fff",
-        backgroundColor: "#f2f4f7",
-      },
-      "&:focus": {
-        backgroundColor: "#fff",
-      },
-      "& input": {
-        padding: "15px 36px 15px 60px",
-      },
-      "&:focus fieldset": {
-        backgroundColor: "#fff",
-        borderColor: "blue",
-      },
-      //   "& fieldset": {
-      //     borderColor: "#f2f4f7",
-      //     backgroundColor: "#f2f4f7",
-      //     // borderRadius: 30,
-      //     height: 50,
-      //   },
-    },
-    "& .MuiFilledInput-root": {
-      "& input": {
-        padding: "15px 36px 15px 60px",
-      },
-      "&:focus fieldset": {
-        borderColor: "blue",
-      },
-    },
-  },
-})(TextField);
-
 export default function Search(props) {
   const classes = useStyles();
+  const stopHideSearch = (e) => {
+    e.stopPropagation();
+  };
 
   return (
     <div className={classes.wrap} onClick={props.hideSearch}>
-      <Container maxwidth={false} className={classes.search}>
-        <form
-          style={{ position: "relative" }}
-          // className={classes.searchForm}
-        >
-          {/* <TextField
-            placeholder="#태그, 회사, 포지션 검색"
-            className={classes.searchInput}
-          /> */}
+      <Container maxwidth={false} className={classes.searchContainer}>
+        <form style={{ position: "relative" }}>
           <SearchTextField
             fullWidth
             placeholder="#태그, 회사, 포지션 검색"
             variant="outlined"
             id="search-input"
-            inputProps={{
-              //   classes: { notchedOutline: classes.noBorder },
-              disableOutline: true,
-            }}
+            onClick={stopHideSearch}
           />
-          {/* <TextField
-            id="filled-full-width"
-            style={{ margin: 8 }}
-            placeholder="Placeholder"
-            fullWidth
-            margin="normal"
-            variant="filled"
-          /> */}
-          <SearchRoundedIcon className={classes.iconSearch} />
+          <SearchRoundedIcon
+            className={classes.iconSearch}
+            onClick={stopHideSearch}
+          />
         </form>
         <div className={classes.tagWrap}>
           <Container maxwidth={false} className={classes.tagContainer}>
@@ -171,19 +162,20 @@ export default function Search(props) {
               href="#"
               underline="none"
               color="inherit"
-              className={classes.moveTagHome}
+              className={classes.moveCorpTagHome}
             >
               기업태그 홈 이동하기
               <NavigateNextRoundedIcon
-                className={classes.moveIcon}
+                className={classes.moveCorpTagIcon}
               ></NavigateNextRoundedIcon>
             </Link>
-            <ul className={classes.tagRecommend}>
+            <ul className={classes.tagRecommendWrap}>
               {tagRecommend.map((item) => (
                 <li>
                   <TagButton
                     href={item.link}
                     style={{ backgroundColor: item.color }}
+                    onClick={stopHideSearch}
                   >
                     {item.tag}
                   </TagButton>
